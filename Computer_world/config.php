0a128e403e546b0a61aab42a6257c87e7c344b3e
unknown <sauravpoojari65@gmail.com>
2024-03-07 00:20:26 +0530

    first commit

---
+++ Computer_world/config.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'admin');
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+
+if ($conn == false) {
+    die('Error:Cannot Connect');
+}
+?>
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ComputerWorld
+# Computer-World-Website
